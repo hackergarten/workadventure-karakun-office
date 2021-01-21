557746a81a71c0cb332486617901658f9f3e5775
grigala <grigalaa@gmail.com>
2021-01-21 16:45:23 +0000

    Deploying to gh-pages from master @ 8b8c6d48d596daadc97e9791ed8444b35f5b6c7b 🚀

--- tilesets/floor.tsx
+++ tilesets/floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="floor" tilewidth="32" tileheight="32" tilecount="1116" columns="36">
- <image source="../../../../Downloads/floor.png" width="1152" height="1008"/>
+ <image source="../tile_images/floor.png" width="1152" height="1008"/>
 </tileset>
